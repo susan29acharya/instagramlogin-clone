f9448a69cd718766574e299d747e46a3012ea9fc
susan acharya <krksusan8@gmail.com>
2024-10-20 19:23:45 +0545

    new login page created

---
+++ app/login/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="body">
+        <div className="flex-container">
+          <div className="left-container">
+            <img src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png" alt=""/>
+            <div className="mobile-photo">
+            <img className="top" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png" alt=""/>
+            </div>
+          </div>
+
+          <div className="right-container"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
