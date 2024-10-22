2133a59523ace6318c1a41bb8c76d927ef47f7e6
susan acharya <krksusan8@gmail.com>
2024-10-22 20:25:33 +0545

    right section form added

--- app/login/page.js
+++ app/login/page.js
@@ -6,13 +6,49 @@ const Login = () => {
       <div className="body">
         <div className="flex-container">
           <div className="left-container">
-            <img src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png" alt=""/>
+            <img
+              src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png"
+              alt=""
+            />
             <div className="mobile-photo">
-            <img className="top" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png" alt=""/>
+              <img
+                className="top"
+                src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png"
+                alt=""
+              />
             </div>
           </div>
 
-          <div className="right-container"></div>
+          <div className="right-container">
+            <div className="form">
+              <div className="insta-logo">
+                <img src="instagram-word.png" />
+              </div>
+
+              <form>
+                <input
+                  type="text"
+                  placeholder="phone number, username, or email"
+                />
+                <br />
+                <input type="password" placeholder="password" />
+
+                <button>Log in</button>
+
+                <div className="or">
+                  <span></span>
+                  <h5>OR</h5>
+                  <span></span>
+                </div>
+
+                <div className="fbsection">
+                  <img src="fblogo.png" />
+                  <p>Log in with Facebook</p>
+                </div>
+                <p>Forgot password?</p>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
